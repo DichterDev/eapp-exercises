75d335da21424e3d29bf0ff3892dc58a840e5ff6
DichterDev <dichterdev.null@gmail.com>
2026-03-16 20:36:48 +0100

    feat: added user cart

--- 01-ecommerce/src/main/java/at/fhv/ecommerce/application/user/handler/UserCommandHandlerService.java
+++ 01-ecommerce/src/main/java/at/fhv/ecommerce/application/user/handler/UserCommandHandlerService.java
@@ -3,9 +3,10 @@ package at.fhv.ecommerce.application.user.handler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import at.fhv.ecommerce.application.common.command.CommandResponse;
+import at.fhv.ecommerce.application.user.command.AddItemToUserCartCommand;
 import at.fhv.ecommerce.application.user.command.RegisterUserCommand;
+import at.fhv.ecommerce.domain.user.model.CartItem;
 import at.fhv.ecommerce.domain.user.model.User;
-import at.fhv.ecommerce.domain.user.model.UserId;
 import at.fhv.ecommerce.domain.user.ports.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -23,5 +24,15 @@ public class UserCommandHandlerService implements UserCommandHandler {
         repository.save(user);
 
         return new CommandResponse(user.getId().value().toString());
+    }
+
+    @Override
+    @Transactional
+    public void handleAddItem(AddItemToUserCartCommand cmd) {
+        var user = repository.findById(cmd.userId()).orElseThrow();
+
+        user.addCartItem(new CartItem(cmd.productId(), cmd.amount()));
+
+        repository.save(user);
     }
 }
--- 01-ecommerce/src/main/java/at/fhv/ecommerce/domain/user/ports/UserRepository.java
+++ 01-ecommerce/src/main/java/at/fhv/ecommerce/domain/user/ports/UserRepository.java
@@ -1,8 +1,14 @@
 package at.fhv.ecommerce.domain.user.ports;
 
+import java.util.List;
+import java.util.Optional;
 import at.fhv.ecommerce.domain.common.BaseRepository;
+import at.fhv.ecommerce.domain.user.model.CartItem;
 import at.fhv.ecommerce.domain.user.model.User;
 import at.fhv.ecommerce.domain.user.model.UserId;
 
 public interface UserRepository extends BaseRepository<User, UserId> {
+    Optional<User> findByIdWithCartItems(UserId id);
+
+    List<CartItem> findCartItemsById(UserId id);
 }
