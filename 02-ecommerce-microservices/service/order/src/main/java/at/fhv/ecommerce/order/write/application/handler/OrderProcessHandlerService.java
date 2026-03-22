141ab26a6f219ff4e12fbe3615de52114a44403c
DichterDev <dichterdev.null@gmail.com>
2026-03-22 19:21:59 +0100

    feat: order rest and backend

--- 02-ecommerce-microservices/service/order/src/main/java/at/fhv/ecommerce/order/write/application/handler/OrderProcessHandlerService.java
+++ 02-ecommerce-microservices/service/order/src/main/java/at/fhv/ecommerce/order/write/application/handler/OrderProcessHandlerService.java
@@ -2,6 +2,8 @@ package at.fhv.ecommerce.order.write.application.handler;
 
 import org.springframework.stereotype.Service;
 import at.fhv.ecommerce.order.read.application.handler.OrderQueryHandler;
+import at.fhv.ecommerce.order.read.application.query.GetOrderById;
+import at.fhv.ecommerce.order.read.application.query.GetOrderDetailById;
 import at.fhv.ecommerce.order.write.application.client.ProductClient;
 import at.fhv.ecommerce.order.write.domain.event.OrderPlaced;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +16,11 @@ public class OrderProcessHandlerService implements OrderProcessHandler {
 
     @Override
     public void handle(OrderPlaced event) {
+        var order = query.getDetail(new GetOrderDetailById(event.orderId().value()));
 
+        order.orderItems().forEach(item -> {
+            product.reduceStock(item.productId(), item.amount());
+        });
     }
 
 }
