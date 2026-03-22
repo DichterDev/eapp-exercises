4f34e927b7062b835098b4e0175d0cd8f98ea6aa
DichterDev <dichterdev.null@gmail.com>
2026-03-22 21:54:17 +0100

    feat: order state change

--- 02-ecommerce-microservices/service/order/src/main/java/at/fhv/ecommerce/order/write/application/handler/OrderCommandHandler.java
+++ 02-ecommerce-microservices/service/order/src/main/java/at/fhv/ecommerce/order/write/application/handler/OrderCommandHandler.java
@@ -1,8 +1,17 @@
 package at.fhv.ecommerce.order.write.application.handler;
 
 import at.fhv.ecommerce.common.application.CommandResponse;
+import at.fhv.ecommerce.order.write.application.command.CancelOrder;
+import at.fhv.ecommerce.order.write.application.command.CompleteOrder;
+import at.fhv.ecommerce.order.write.application.command.FailOrder;
 import at.fhv.ecommerce.order.write.application.command.PlaceOrder;
 
 public interface OrderCommandHandler {
     CommandResponse place(PlaceOrder cmd);
+
+    void complete(CompleteOrder cmd);
+
+    void fail(FailOrder cmd);
+
+    void cancel(CancelOrder cmd);
 }
