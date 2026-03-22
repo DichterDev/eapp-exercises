33766ed5c46d9bf07decac637407b89fb2d1bcc4
DichterDev <dichterdev.null@gmail.com>
2026-03-22 12:14:34 +0100

    init: gradle config

--- 02-ecommerce-microservices/common/src/main/java/at/fhv/ecommerce/common/Application.java
+++ 02-ecommerce-microservices/common/src/main/java/at/fhv/ecommerce/common/Application.java
@@ -1,4 +1,4 @@
-package at.fhv.ecommerce;
+package at.fhv.ecommerce.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
