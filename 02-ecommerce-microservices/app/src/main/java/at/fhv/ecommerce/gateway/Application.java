171efa83b49f1be8864069fa6850f0e25a22368e
DichterDev <dichterdev.null@gmail.com>
2026-03-22 16:45:43 +0100

    feat: working docker, build broken

--- 02-ecommerce-microservices/app/src/main/java/at/fhv/ecommerce/gateway/Application.java
+++ 02-ecommerce-microservices/app/src/main/java/at/fhv/ecommerce/gateway/Application.java
@@ -2,7 +2,13 @@ package at.fhv.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+// @SpringBootApplication(exclude = {
+//     DataSourceAutoConfiguration.class,
+//     HibernateJpaAutoConfiguration.class
+// })
 @SpringBootApplication
 public class Application {
 
