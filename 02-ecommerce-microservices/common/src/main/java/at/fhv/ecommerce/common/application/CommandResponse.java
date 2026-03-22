63d5a6863918ee2fa7aff9deb3fc65bf7db7147c
DichterDev <dichterdev.null@gmail.com>
2026-03-22 13:29:57 +0100

    feat: common module

---
+++ 02-ecommerce-microservices/common/src/main/java/at/fhv/ecommerce/common/application/CommandResponse.java
@@ -0,0 +1,4 @@
+package at.fhv.ecommerce.common.application;
+
+public record CommandResponse(String id) {
+}
---
+++ 02-ecommerce-microservices/common/src/main/java/at/fhv/ecommerce/common/infrastructure/persistence/MoneyMapper.java
@@ -0,0 +1,25 @@
+package at.fhv.ecommerce.common.infrastructure.persistence;
+
+import java.util.Currency;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import at.fhv.ecommerce.common.domain.Money;
+
+@Mapper
+public interface MoneyMapper {
+    @Mapping(target = "value", source = "amount")
+    @Mapping(target = "add", ignore = true)
+    @Mapping(target = "subtract", ignore = true)
+    Money toMoney(MoneyEmbed embed);
+
+    @Mapping(target = "amount", source = "value")
+    MoneyEmbed toEmbed(Money money);
+
+    default String map(Currency currency) {
+        return currency != null ? currency.getCurrencyCode() : null;
+    }
+
+    default Currency map(String currency) {
+        return currency != null ? Currency.getInstance(currency) : null;
+    }
+}
