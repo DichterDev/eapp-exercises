e107bd39382084d984dd9071a1ae33e9781e8445
DichterDev <dichterdev.null@gmail.com>
2026-03-22 17:08:53 +0100

    fix: build fix, bumped spring version

--- 02-ecommerce-microservices/app/src/main/java/at/fhv/ecommerce/gateway/Application.java
+++ 02-ecommerce-microservices/app/src/main/java/at/fhv/ecommerce/gateway/Application.java
@@ -2,14 +2,13 @@ package at.fhv.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-// @SpringBootApplication(exclude = {
-//     DataSourceAutoConfiguration.class,
-//     HibernateJpaAutoConfiguration.class
-// })
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
