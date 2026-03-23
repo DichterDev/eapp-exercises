3d2a96b58995db834f5d45ccbaa561e245c27326
DichterDev <dichterdev.null@gmail.com>
2026-03-23 13:51:53 +0100

    init: common module

---
+++ 03-ecommerce-messaging/common/src/main/java/at/fhv/common/domain/BaseDomainRoot.java
@@ -0,0 +1,19 @@
+package at.fhv.common.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseDomainRoot {
+    private final List<DomainEvent> events = new ArrayList<>();
+
+    protected void registerEvent(DomainEvent event) {
+        this.events.add(event);
+    }
+
+    public List<DomainEvent> pullEvents() {
+        List<DomainEvent> cleared = List.copyOf(events);
+        this.events.clear();
+
+        return cleared;
+    }
+}
