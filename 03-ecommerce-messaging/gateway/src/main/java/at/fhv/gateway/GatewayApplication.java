c2fc7cbadf5046adfea813760aae63d4de9bb248
DichterDev <dichterdev.null@gmail.com>
2026-03-23 13:41:07 +0100

    feat: init modules

---
+++ 03-ecommerce-messaging/gateway/src/main/java/at/fhv/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
