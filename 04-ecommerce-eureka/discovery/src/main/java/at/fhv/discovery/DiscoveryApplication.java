2507d5fa95ee814e64bfd79e412a3dceb47e738c
DichterDev <dichterdev.null@gmail.com>
2026-04-01 19:23:43 +0200

    04: discovery properties

--- 04-ecommerce-eureka/discovery/src/main/java/at/fhv/discovery/DiscoveryApplication.java
+++ 04-ecommerce-eureka/discovery/src/main/java/at/fhv/discovery/DiscoveryApplication.java
@@ -2,9 +2,11 @@ package at.fhv.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication()
 @EnableEurekaServer
 public class DiscoveryApplication {
     public static void main(String[] args) {
