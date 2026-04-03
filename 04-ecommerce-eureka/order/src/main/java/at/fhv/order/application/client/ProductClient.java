b1700e4a7288feb7c97bbea626ed1696d9e780b5
DichterDev <dichterdev.null@gmail.com>
2026-04-03 11:47:41 +0200

    04: docker config working

--- 04-ecommerce-eureka/order/src/main/java/at/fhv/order/application/client/ProductClient.java
+++ 04-ecommerce-eureka/order/src/main/java/at/fhv/order/application/client/ProductClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient
+@FeignClient(name = "product-service")
 public interface ProductClient {
     @GetMapping("/products/prices")
     Map<UUID, Double> getPrices(@RequestBody List<UUID> productIds);
