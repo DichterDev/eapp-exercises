5d5373a03809e64ff3057829e02ed6469961e25f
DichterDev <dichterdev.null@gmail.com>
2026-04-04 15:08:39 +0200

    04: kafka wip

--- 04-ecommerce-eureka/order/src/main/java/at/fhv/order/infrastructure/messaging/KafkaOrderEventPublisher.java
+++ 04-ecommerce-eureka/order/src/main/java/at/fhv/order/infrastructure/messaging/KafkaOrderEventPublisher.java
@@ -1,6 +1,7 @@
 package at.fhv.order.infrastructure.messaging;
 
 import at.fhv.common.domain.event.DomainEvent;
+import at.fhv.common.domain.event.order.OrderPlaced;
 import at.fhv.order.domain.port.OrderEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.function.StreamBridge;
@@ -13,11 +14,14 @@ import java.util.List;
 public class KafkaOrderEventPublisher implements OrderEventPublisher {
     private final StreamBridge stream;
 
-
     @Override
     public void publishAll(List<DomainEvent> events) {
         for (DomainEvent event : events) {
-            stream.send("kafka-order-event", event);
+            if (event instanceof OrderPlaced) {
+                stream.send("kafka-order-placed-out-0", event);
+            } else {
+                stream.send("kafka-order-event-out-0", event);
+            }
         }
     }
 }
